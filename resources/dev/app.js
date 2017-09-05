d2fe1658c72f864f40c6d00ef4162bf7efc190e4
Robert Biggs <rbiggs@ymail.com>
2017-09-04 20:50:00 -0700

    New project build fix.

--- resources/dev/app.js
+++ resources/dev/app.js
@@ -1,19 +1,19 @@
 import {h, Component} from 'composi'
 
-(() => {
-  const title = new Component({
-    root: 'header',
-    state: 'World',
-    render: (message) => (
-      <h1>Hello, {message}!</h1>
-    ),
-    styles: {
-      padding: 20,
-      backgroundColor: '#333',
-      h1: {
-        color: '#fff',
-        margin: 0
-      }
+
+const title = new Component({
+  root: 'header',
+  state: 'World',
+  render: (message) => (
+    <h1>Hello, {message}!</h1>
+  ),
+  styles: {
+    padding: 20,
+    backgroundColor: '#333',
+    h1: {
+      color: '#fff',
+      margin: 0
     }
-  })
+  }
 })
+title.update()
