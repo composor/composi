debb7e71ba533cf8b3b00b0472a8b663af9c7ec0
Robert Biggs <rbiggs@ymail.com>
2017-11-02 14:02:41 -0700

    Cleaned up styles for default project.

--- resources/dev/title.js
+++ resources/dev/title.js
@@ -3,18 +3,8 @@ import {h, Component} from 'composi'
 export const title = new Component({
   container: 'header',
   render: (message) => {
-    const styles = `
-    nav {
-      padding: 20px;
-      background-color: '#333';
-    }
-    h1 {
-      color: '#fff';
-      margin: 0;
-    }`
     return (
       <nav>
-        <style>{styles}</style>
         <h1>Hello, {message}!</h1>
       </nav>
     )
